7e049c670cc6f9d2d4dcec3fd9e2a0d690b6f1cd
bguilas <bryanjguilas@gmail.com>
2020-11-21 16:49:32 -0800

    started backend

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+//MiddleWare
+app.use(cors());
+app.use(express.json());
+
+//MongoDB Connection
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true});
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDb database connection established");
+})
+
+//Routes
+const exercisesRouter = require('./routes/exercises');
+const userRouter = require('./routes/users');
+
+app.use('exercises', exercisesRouter);
+app.use('users', userRouter);
+
+//Express Server
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
+
